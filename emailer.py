950077c4e146562f88e391c04b082d28305ddf6d
Meuly <davemeulensteen@gmail.com>
2026-02-04 13:01:59 -0500

    Update emailer.py

--- emailer.py
+++ emailer.py
@@ -1,1 +1,14 @@
+import smtplib
+from email.mime.text import MIMEText
+from config import SMTP_SERVER, SMTP_PORT
 
+def send_email(subject, body, username, password, sender, recipient):
+    msg = MIMEText(body)
+    msg["Subject"] = subject
+    msg["From"] = sender
+    msg["To"] = recipient
+
+    with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
+        server.starttls()
+        server.login(username, password)
+        server.send_message(msg)
