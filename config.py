ba4ff435e080ee900d3c66dc25ca7d80685e6d1f
Meuly <davemeulensteen@gmail.com>
2026-02-04 13:00:54 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
+EMAIL_FROM = "davemeulensteen@gmail.com"
+EMAIL_TO = "davemeulensteen@gmail.com"
 
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 587
+
+# These will be GitHub Secrets (not hard-coded)
+EMAIL_USERNAME = None
+EMAIL_PASSWORD = None
